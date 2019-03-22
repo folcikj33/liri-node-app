b9d3bd0fe4bc34975e8a4c320d00f0267e6e9418
Jake <folcik.jake@gmail.com>
2019-03-22 15:23:41 -0500

    actually some more file structure added

---
+++ liri.js
@@ -0,0 +1,9 @@
+js;
+require("dotenv").config();
+
+// links to keys folder; ./ required because I made this file
+js;
+var keys = require("./keys.js");
+
+js;
+var spotify = new Spotify(keys.spotify);
