b4ea8e4e4124ddeb80086301846359d9e8137572
Jake <folcik.jake@gmail.com>
2019-03-22 16:15:15 -0500

    pseudocode added

--- liri.js
+++ liri.js
@@ -1,9 +1,58 @@
-js;
-require("dotenv").config();
+// --------------------------------------------------------------------------------
+// these functions will call all the modules that I'll need for all the various
+// parts of the app to to function:
+// () obtain api keys
+// () double-check each of them are installed to the folder downloaded
+// () they are all operating
+// --------------------------------------------------------------------------------
 
-// links to keys folder; ./ required because I made this file
-js;
-var keys = require("./keys.js");
+// this module should have top priority beacuse
+// it's why we're on the back-end to begin with--->
+// js;
+// require("dotenv").config();
 
-js;
-var spotify = new Spotify(keys.spotify);
+// links to keys folder; the `./` and `.js` are required because I made this
+// js;
+// var keys = require("./keys.js");
+
+// the fun part where asks the user a series of questions
+// still need: require("inquirer")
+
+// js;
+// var spotify = new Spotify(keys.spotify);
+
+// still need: OMDB API
+
+// still need: concert this API
+
+// still need: what it says.api
+
+// still need: require("axios")
+
+// --------------------------------------------------------------------------------
+// set up user command choice using inquirer:
+// () get inquirer to operate properly -- first question and then console log
+// () set up series of switch to determine next inquiry
+// () every path works with a console-log
+// () every path calls respective function
+// --------------------------------------------------------------------------------
+//  Paths:
+// --------------------------------------------------------------------------------
+// Question 1: What are you looking for?:
+// 1. concert-this
+// 2. spotify-this-song
+// 3. movie-this
+// 4. do-what it says.
+
+// If 1: (except use 'switch' function for challenge)
+
+// --------------------------------------------------------------------------------
+// whichever one they pick will effect what the next question from the inquirer asks
+// them through a series of switch commands
+// --------------------------------------------------------------------------------
+
+//
+
+// --------------------------------------------------------------------------------
+//
+// --------------------------------------------------------------------------------
